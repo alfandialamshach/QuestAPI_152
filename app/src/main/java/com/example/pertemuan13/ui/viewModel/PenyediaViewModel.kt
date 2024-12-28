6fca7ba4fefaeebd0b51890de286acbd7c5d1ac7
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-29 01:56:41 +0700

    Menambahakan DetailMhsViewModel pada PenyediaViewModel

--- app/src/main/java/com/example/pertemuan13/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/viewModel/PenyediaViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.pertemuan13.ui.viewModel
 
+import DetailMhsViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -10,6 +12,7 @@ object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer { HomeViewModel(AplikasiMahasiswa().container.mahasiswaRepository) }
         initializer { InsertViewModel(AplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { DetailMhsViewModel(createSavedStateHandle(),AplikasiMahasiswa().container.mahasiswaRepository) }
     }
 
     fun CreationExtras.AplikasiMahasiswa():MahasiswaApplications =
