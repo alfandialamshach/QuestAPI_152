707ae2148bede79ab7ec5ef6e15121c5bfa85f77
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-28 20:35:42 +0700

    menambahakan kode class InsertViewModel pada InsertViewModel.kt

--- app/src/main/java/com/example/pertemuan13/ui/viewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/viewModel/InsertViewModel.kt
@@ -1,6 +1,31 @@
 package com.example.pertemuan13.ui.viewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan13.model.Mahasiswa
+import com.example.pertemuan13.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent =insertUiEvent)
+    }
+
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
