8dcf8dd92e22074cf955fde9ad7820265ffc4e42
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-28 20:26:54 +0700

    Membetulkan error MahasiswaRepository

--- app/src/main/java/com/example/pertemuan13/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/pertemuan13/repository/MahasiswaRepository.kt
@@ -4,17 +4,22 @@ import android.os.Parcel
 import android.os.Parcelable
 import com.example.pertemuan13.model.Mahasiswa
 import com.example.pertemuan13.service_api.MahasiswaService
+import okio.IOException
 
 interface MahasiswaRepository{
     suspend fun getAllMahasiswa(): List<Mahasiswa>
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
     suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
     suspend fun deleteMahasiswa(nim: String)
-    suspend fun geMahasiswaById(nim: String) : Mahasiswa
+    suspend fun getMahasiswaByNim(nim: String) : Mahasiswa
 }
+
 class NetworkMahasiswaRepository(
     private val mahasiswaApiService: MahasiswaService
 ) : MahasiswaRepository{
+
+    override suspend fun getAllMahasiswa(): List<Mahasiswa> =
+        mahasiswaApiService.getAllMahasiswa()
 
     override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
         mahasiswaApiService.insertMahasiswa(mahasiswa)
@@ -27,20 +32,17 @@ class NetworkMahasiswaRepository(
     override suspend fun deleteMahasiswa(nim: String) {
         try{
             val response = mahasiswaApiService.deleteMahasiswa(nim)
-            if (!response.isSuccessful)
+            if (!response.isSuccessful){
                 throw IOException("Failed to delete Mahasiswa. HTTP Status Code: " +
                         "${response.code()}")
         } else {
-            response.message()
-            println(response.message())
-        }catch (e:Expection){
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
             throw e
         }
     }
-
-
-    override suspend fun getMahasiswa(): List<Mahasiswa>
-    mahasiswaApiService.getAllMahasiswa()
 
 
     override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
