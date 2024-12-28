df7ced2031788032bd3daec2070be32d544d914f
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-29 01:53:31 +0700

    memperbaiki port pada baseUrl

--- app/src/main/java/com/example/pertemuan13/Container/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan13/Container/MahasiswaContainer.kt
@@ -14,7 +14,7 @@ interface AppContainer{
 
 class MahasiswaContainer : AppContainer{
 
-    private val baseUrl = "http://10.0.2.2:800/umyTI/" //localhost diganti ip kalo run dihp
+    private val baseUrl = "http://10.0.2.2:80/umyTI/" //localhost diganti ip kalo run dihp
     private val json = Json {ignoreUnknownKeys = true}
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
