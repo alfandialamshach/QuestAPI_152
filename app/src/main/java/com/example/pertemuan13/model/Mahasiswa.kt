f94abb6535380dfc49bfba5ebab755c9bfc0dd52
Alfandi Alamshach <fandixpc1@gmail.com>
2024-12-28 14:46:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertemuan13/model/Mahasiswa.kt
@@ -0,0 +1,21 @@
+package com.example.pertemuan13.model
+
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import kotlinx.serialization.descriptors.*
+import kotlinx.serialization.encoding.*
+
+
+@Serializable
+data class Mahasiswa (
+    val nim: String,
+    val nama: String,
+    val alamat: String,
+
+    @SerialName("jenis_kelamin")
+    val jenisKelamin: String,
+
+    val kelas: String,
+    val angkatan: String
+)
---
+++ app/src/main/java/com/example/pertemuan13/repository/MahasiswaRepository.kt
@@ -0,0 +1,50 @@
+package com.example.pertemuan13.repository
+
+import android.os.Parcel
+import android.os.Parcelable
+import com.example.pertemuan13.model.Mahasiswa
+import com.example.pertemuan13.service_api.MahasiswaService
+
+interface MahasiswaRepository{
+    suspend fun getAllMahasiswa(): List<Mahasiswa>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(nim: String)
+    suspend fun geMahasiswaById(nim: String) : Mahasiswa
+}
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository{
+
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try{
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful)
+                throw IOException("Failed to delete Mahasiswa. HTTP Status Code: " +
+                        "${response.code()}")
+        } else {
+            response.message()
+            println(response.message())
+        }catch (e:Expection){
+            throw e
+        }
+    }
+
+
+    override suspend fun getMahasiswa(): List<Mahasiswa>
+    mahasiswaApiService.getAllMahasiswa()
+
+
+    override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswabyNim(nim)
+    }
+
+}
